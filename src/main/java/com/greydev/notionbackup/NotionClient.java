3fa183859662408bcd9803fc6f4cfe97f5986e47
Davide Tedesco <davide_hrd@hotmail.it>
2022-08-22 00:33:15 +0200

    Update NotionClient.java

--- src/main/java/com/greydev/notionbackup/NotionClient.java
+++ src/main/java/com/greydev/notionbackup/NotionClient.java
@@ -215,7 +215,7 @@ public class NotionClient {
 				.POST(HttpRequest.BodyPublishers.ofString(postBody))
 				.build();
 
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 800; i++) {
 			HttpResponse<String> response = newClient.send(request, HttpResponse.BodyHandlers.ofString());
 
 			// TODO Need to prepare Jackson Document and see how this is handled. I don't wan't this wrapper "Results" class
